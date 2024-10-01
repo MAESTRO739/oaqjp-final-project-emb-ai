7c2bb58d3a738ea9750575169a11a6ef6f0605d1
MAESTRO739 <butakova.tatyana79@gmail.com>
2024-10-01 18:09:57 -0400

    completed task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,4 +7,22 @@ def emotion_detector(text_to_analyse):
     obj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url=url, headers=headers, json=obj)
     formatted_response = json.loads(response.text)
-    return formatted_response
+
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+           }
